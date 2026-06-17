dfae79318b01a65d3d5b16a57cebf02dd20576a0
ekaterina-despotova <ekaterina.despotova@gmail.com>
2026-06-17 09:46:58 +0300

    chore: delete unneccessary doc file

--- pkg/recommendation/recommender/carbonrightsize/doc.go
+++
@@ -1,5 +0,0 @@
-// Package carbonrightsize implements the CarbonRightSizing recommender plugin.
-// It right-sizes CPU and memory resources for pods that are energy-inefficient,
-// using Kepler energy metrics from Prometheus to compute an energy-efficiency
-// ratio and recommending requests/limits based on historical usage percentiles.
-package carbonrightsize
